a7d8a7c75e46d18e50c46cc83d5d035dd3aea959
Guilherme <alves.guilherme1357@gmail.com>
2020-01-14 15:13:23 -0300

    implementação da biblioteca textdistance

--- allFunc.py
+++ allFunc.py
@@ -1,4 +1,4 @@
-import csv, ast
+import csv, ast, textdistance
 from allClass import *
 
 def dlLoad(nivel,area):
@@ -16,8 +16,8 @@ def dlLoad(nivel,area):
                     if aux[0] is '[' and aux[-1] is ']':            
                         gradList = ast.literal_eval(aux)
                         for i in gradList:
-                            result = damerau_levenshtein_distance(i,area)
-                            if result < 6:
+                            result = textdistance.damerau_levenshtein.normalized_distance(i,area)
+                            if result < 0.5:
                                 dados = Info(linha["nome"])
                                 dados.setGrad(i)
                                 dados.setReg(contador)
@@ -25,8 +25,8 @@ def dlLoad(nivel,area):
                                 contador += 1
                     #Caso possua apenas uma graduação...
                     else:
-                        result = damerau_levenshtein_distance(aux,area)
-                        if result < 6:
+                        result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
+                        if result < 0.5:
                             dados = Info(linha["nome"])
                             dados.setGrad(aux)
                             dados.setReg(contador)
@@ -44,8 +44,8 @@ def dlLoad(nivel,area):
                     if aux[0] is '[' and aux[-1] is ']':            
                         especList = ast.literal_eval(aux)
                         for i in especList:
-                            result = damerau_levenshtein_distance(i,area)
-                            if result < 6:
+                            result = textdistance.damerau_levenshtein.normalized_distance(i,area)
+                            if result < 0.5:
                                 dados = Info(linha["nome"])
                                 dados.setEspec(i)
                                 dados.setReg(contador)
@@ -53,8 +53,8 @@ def dlLoad(nivel,area):
                                 contador += 1
             
                     else:
-                        result = damerau_levenshtein_distance(aux,area)
-                        if result < 6:
+                        result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
+                        if result < 0.5:
                             dados = Info(linha["nome"])
                             dados.setEspec(aux)
                             dados.setReg(contador)
@@ -70,8 +70,8 @@ def dlLoad(nivel,area):
                     if aux[0] is '[' and aux[-1] is ']':            
                         mestList = ast.literal_eval(aux)
                         for i in mestList:
-                            result = damerau_levenshtein_distance(i,area)
-                            if result < 6:
+                            result = textdistance.damerau_levenshtein.normalized_distance(i,area)
+                            if result < 0.5:
                                 dados = Info(linha["nome"])
                                 dados.setMest(i)
                                 dados.setReg(contador)
@@ -79,8 +79,8 @@ def dlLoad(nivel,area):
                                 contador += 1
             
                     else:
-                        result = damerau_levenshtein_distance(aux,area)
-                        if result < 6:
+                        result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
+                        if result < 0.5:
                             dados = Info(linha["nome"])
                             dados.setMest(aux)
                             dados.setReg(contador)
@@ -96,8 +96,8 @@ def dlLoad(nivel,area):
                     if aux[0] is '[' and aux[-1] is ']':            
                         doutList = ast.literal_eval(aux)
                         for i in doutList:
-                            result = damerau_levenshtein_distance(i,area)
-                            if result < 6:
+                            result = textdistance.damerau_levenshtein.normalized_distance(i,area)
+                            if result < 0.5:
                                 dados = Info(linha["nome"])
                                 dados.setDout(i)
                                 dados.setReg(contador)
@@ -105,8 +105,8 @@ def dlLoad(nivel,area):
                                 contador += 1
             
                     else:
-                        result = damerau_levenshtein_distance(aux,area)
-                        if result < 6:
+                        result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
+                        if result < 0.5:
                             dados = Info(linha["nome"])
                             dados.setDout(aux)
                             dados.setReg(contador)
@@ -122,8 +122,8 @@ def dlLoad(nivel,area):
                     if aux[0] is '[' and aux[-1] is ']':            
                         posDoutList = ast.literal_eval(aux)
                         for i in posDoutList:
-                            result = damerau_levenshtein_distance(i,area)
-                            if result < 6:
+                            result = textdistance.damerau_levenshtein.normalized_distance(i,area)
+                            if result < 0.5:
                                 dados = Info(linha["nome"])
                                 dados.setPosDout(i)
                                 dados.setReg(contador)
@@ -131,8 +131,8 @@ def dlLoad(nivel,area):
                                 contador += 1
             
                     else:
-                        result = damerau_levenshtein_distance(aux,area)
-                        if result < 6:
+                        result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
+                        if result < 0.5:
                             dados = Info(linha["nome"])
                             dados.setPosDout(aux)
                             dados.setReg(contador)
@@ -143,6 +143,151 @@ def dlLoad(nivel,area):
                  
             
         print(showData(lista,nivel))
+'''
+def jdLoad(nivel,area):
+    with open('curriculos-lattes.csv') as dataset:
+        leitor_dataset = csv.DictReader(dataset,delimiter=',')
+        contador = 1
+        lista = LinkedList()
+
+        #Para nível de graduação
+        if nivel == 'GRADUAÇÃO':
+            for linha in leitor_dataset:
+                if linha["graduacao"] == 'SIM' and linha["area_graduacao"]:
+                    aux = linha["area_graduacao"]
+                    #Caso possua mais de uma graduação converte de string p/ lista
+                    if aux[0] is '[' and aux[-1] is ']':            
+                        gradList = ast.literal_eval(aux)
+                        for i in gradList:
+                            result = textdistance.jaro.distance(i,area)
+                            if result <= 0.3:
+                                dados = Info(linha["nome"])
+                                dados.setGrad(i)
+                                dados.setReg(contador)
+                                lista.insertion(contador,dados)
+                                contador += 1
+                    #Caso possua apenas uma graduação...
+                    else:
+                        result = textdistance.jaro.distance(aux,area)
+                        if result <= 0.3:
+                            dados = Info(linha["nome"])
+                            dados.setGrad(aux)
+                            dados.setReg(contador)
+                            lista.insertion(contador,dados)
+                            contador += 1
+                else:
+                    continue    
+        #A partir daqui os códigos seguem semelhantes mudando o nível de conhecimento, nome de variáveis e
+        #informações inseridas na classe Info
+
+        elif nivel == 'ESPECIALIZAÇÃO':
+            for linha in leitor_dataset:
+                if linha["especializacao"] == 'SIM' and linha["area_especializacao"]:
+                    aux = linha["area_especializacao"]
+                    if aux[0] is '[' and aux[-1] is ']':            
+                        especList = ast.literal_eval(aux)
+                        for i in especList:
+                            result = textdistance.damerau_levenshtein.normalized_distance(i,area)
+                            if result < 0.3:
+                                dados = Info(linha["nome"])
+                                dados.setEspec(i)
+                                dados.setReg(contador)
+                                lista.insertion(contador,dados)
+                                contador += 1
+            
+                    else:
+                        result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
+                        if result < 0.3:
+                            dados = Info(linha["nome"])
+                            dados.setEspec(aux)
+                            dados.setReg(contador)
+                            lista.insertion(contador,dados)
+                            contador += 1
+                else:
+                    continue     
+
+        elif nivel == 'MESTRADO':
+            for linha in leitor_dataset:
+                if linha["mestrado"] == 'SIM' and linha["area_mestrado"]:
+                    aux = linha["area_mestrado"]
+                    if aux[0] is '[' and aux[-1] is ']':            
+                        mestList = ast.literal_eval(aux)
+                        for i in mestList:
+                            result = textdistance.damerau_levenshtein.normalized_distance(i,area)
+                            if result < 0.3:
+                                dados = Info(linha["nome"])
+                                dados.setMest(i)
+                                dados.setReg(contador)
+                                lista.insertion(contador,dados)
+                                contador += 1
+            
+                    else:
+                        result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
+                        if result < 0.3:
+                            dados = Info(linha["nome"])
+                            dados.setMest(aux)
+                            dados.setReg(contador)
+                            lista.insertion(contador,dados)
+                            contador += 1
+                else:
+                    continue     
+                    
+        elif nivel == 'DOUTORADO':
+            for linha in leitor_dataset:
+                if linha["doutorado"] == 'SIM' and linha["area_doutorado"]:
+                    aux = linha["area_doutorado"]
+                    if aux[0] is '[' and aux[-1] is ']':            
+                        doutList = ast.literal_eval(aux)
+                        for i in doutList:
+                            result = textdistance.damerau_levenshtein.normalized_distance(i,area)
+                            if result < 0.3:
+                                dados = Info(linha["nome"])
+                                dados.setDout(i)
+                                dados.setReg(contador)
+                                lista.insertion(contador,dados)
+                                contador += 1
+            
+                    else:
+                        result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
+                        if result < 0.3:
+                            dados = Info(linha["nome"])
+                            dados.setDout(aux)
+                            dados.setReg(contador)
+                            lista.insertion(contador,dados)
+                            contador += 1
+                else:
+                    continue 
+
+        elif nivel == 'PÓS-DOUTORADO':
+            for linha in leitor_dataset:
+                if linha["pos_doutorado"] == 'SIM' and linha["area_pos_doutorado"]:
+                    aux = linha["area_pos_doutorado"]
+                    if aux[0] is '[' and aux[-1] is ']':            
+                        posDoutList = ast.literal_eval(aux)
+                        for i in posDoutList:
+                            result = textdistance.damerau_levenshtein.normalized_distance(i,area)
+                            if result < 0.3:
+                                dados = Info(linha["nome"])
+                                dados.setPosDout(i)
+                                dados.setReg(contador)
+                                lista.insertion(contador,dados)
+                                contador += 1
+            
+                    else:
+                        result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
+                        if result < 0.3:
+                            dados = Info(linha["nome"])
+                            dados.setPosDout(aux)
+                            dados.setReg(contador)
+                            lista.insertion(contador,dados)
+                            contador += 1
+                else:
+                    continue                     
+                 
+            
+        print(showData(lista,nivel))
+'''
+
 
 def showData(linkedList,nivel):
     all = linkedList.getHead()
@@ -167,36 +312,12 @@ def showData(linkedList,nivel):
             if all.getNext():
                 all = all.getNext()
             else:
-                break
+                return '=' * 50               
+                
     except AssertionError:
         raise MyException('Não há registros compatíveis com a respectiva área de conhecimento.')
     except:
         raise
-            
-def damerau_levenshtein_distance(s1, s2):
-    d = {}
-    lenstr1 = len(s1)
-    lenstr2 = len(s2)
-    for i in range(-1,lenstr1+1):
-        d[(i,-1)] = i+1
-    for j in range(-1,lenstr2+1):
-        d[(-1,j)] = j+1
-
-    for i in range(lenstr1):
-        for j in range(lenstr2):
-            if s1[i] == s2[j]:
-                cost = 0
-            else:
-                cost = 1
-            d[(i,j)] = min(
-                           d[(i-1,j)] + 1, # deletion
-                           d[(i,j-1)] + 1, # insertion
-                           d[(i-1,j-1)] + cost, # substitution
-                          )
-            if i and j and s1[i]==s2[j-1] and s1[i-1] == s2[j]:
-                d[(i,j)] = min (d[(i,j)], d[i-2,j-2] + cost) # transposition
-
-    return d[lenstr1-1,lenstr2-1]
 
 def menu():
     print('(c) Carregar dados\n(e) Estatísticas da base de dados\n(i) Busca\n(s) Sobre\n(q) Sair')
@@ -207,7 +328,11 @@ def main():
     while True:
         if option == 'i':
             print()
+            qualNv = ('GRADUAÇÃO','ESPECIALIZAÇÃO','MESTRADO','DOUTORADO','PÓS-DOUTORADO')
             qual = input('Qualificação:', ).upper()
+            while qual not in qualNv:
+                print('\nQualificação Inválida\nDisponíveis: Graduação | Especialização | Mestrado | Doutorado | Pós-Doutorado')
+                qual = input('Nova Qualificação:', ).upper()
             area = input('Área do Conhecimento:', )
             try:
                 dlLoad(qual,area)
