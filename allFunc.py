ede4cb38f29a9d36eb62e86bba14cc18bc883512
Guilherme <alves.guilherme1357@gmail.com>
2020-01-19 10:21:47 -0300

    algumas melhorias

--- allFunc.py
+++ allFunc.py
@@ -7,35 +7,42 @@ def damerau(nivel,area):
         contador = 1
         lista = LinkedList()
 
-        #Para nível de graduação
+        #Verificação do nível de conhecimento
         if nivel == 'GRADUAÇÃO':
             for linha in leitor_dataset:
+                #Caso a linha atual possua graduação
                 if linha["graduacao"] == 'SIM' and linha["area_graduacao"]:
                     aux = linha["area_graduacao"]
                     #Caso possua mais de uma graduação converte de string p/ lista
                     if aux[0] is '[' and aux[-1] is ']':            
                         gradList = ast.literal_eval(aux)
+                        #Percorre a lista de graduações
                         for i in gradList:
+                            #Distância de edição
                             result = textdistance.damerau_levenshtein.normalized_distance(i,area)
-                            if result <= 0.4:
+                            if result <= 0.33:
+                                #Cria uma instância da classe Info e insere na lista
                                 dados = Info(linha["nome"])
                                 dados.setGrad(i)
                                 dados.setReg(contador)
                                 lista.insertion(contador,dados)
                                 contador += 1
-                    #Caso possua apenas uma graduação...
+                    #Caso possua apenas uma graduação
                     else:
+                        #Distância de edição
                         result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
-                        if result <= 0.4:
+                        if result <= 0.33:
+                            #Cria uma instância da classe Info e insere na lista
                             dados = Info(linha["nome"])
                             dados.setGrad(aux)
                             dados.setReg(contador)
                             lista.insertion(contador,dados)
                             contador += 1
                 else:
+                    #Caso a linha atual não possua graduação
                     continue    
-        #A partir daqui os códigos seguem semelhantes mudando o nível de conhecimento, nome de variáveis e
-        #informações inseridas na classe Info
+        #A partir daqui os códigos seguem iguais para os outros níveis de formação alterando nome de variáveis,
+        #informações inseridas na classe Info etc
 
         elif nivel == 'ESPECIALIZAÇÃO':
             for linha in leitor_dataset:
@@ -45,7 +52,7 @@ def damerau(nivel,area):
                         especList = ast.literal_eval(aux)
                         for i in especList:
                             result = textdistance.damerau_levenshtein.normalized_distance(i,area)
-                            if result <= 0.5:
+                            if result <= 0.33:
                                 dados = Info(linha["nome"])
                                 dados.setEspec(i)
                                 dados.setReg(contador)
@@ -54,7 +61,7 @@ def damerau(nivel,area):
             
                     else:
                         result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
-                        if result <= 0.5:
+                        if result <= 0.33:
                             dados = Info(linha["nome"])
                             dados.setEspec(aux)
                             dados.setReg(contador)
@@ -71,7 +78,7 @@ def damerau(nivel,area):
                         mestList = ast.literal_eval(aux)
                         for i in mestList:
                             result = textdistance.damerau_levenshtein.normalized_distance(i,area)
-                            if result <= 0.4:
+                            if result <= 0.33:
                                 dados = Info(linha["nome"])
                                 dados.setMest(i)
                                 dados.setReg(contador)
@@ -80,7 +87,7 @@ def damerau(nivel,area):
             
                     else:
                         result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
-                        if result <= 0.4:
+                        if result <= 0.33:
                             dados = Info(linha["nome"])
                             dados.setMest(aux)
                             dados.setReg(contador)
@@ -97,7 +104,7 @@ def damerau(nivel,area):
                         doutList = ast.literal_eval(aux)
                         for i in doutList:
                             result = textdistance.damerau_levenshtein.normalized_distance(i,area)
-                            if result <= 0.4:
+                            if result <= 0.33:
                                 dados = Info(linha["nome"])
                                 dados.setDout(i)
                                 dados.setReg(contador)
@@ -106,7 +113,7 @@ def damerau(nivel,area):
             
                     else:
                         result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
-                        if result <= 0.4:
+                        if result <= 0.33:
                             dados = Info(linha["nome"])
                             dados.setDout(aux)
                             dados.setReg(contador)
@@ -123,7 +130,7 @@ def damerau(nivel,area):
                         posDoutList = ast.literal_eval(aux)
                         for i in posDoutList:
                             result = textdistance.damerau_levenshtein.normalized_distance(i,area)
-                            if result <= 0.4:
+                            if result <= 0.33:
                                 dados = Info(linha["nome"])
                                 dados.setPosDout(i)
                                 dados.setReg(contador)
@@ -132,7 +139,7 @@ def damerau(nivel,area):
             
                     else:
                         result = textdistance.damerau_levenshtein.normalized_distance(aux,area)
-                        if result <= 0.4:
+                        if result <= 0.33:
                             dados = Info(linha["nome"])
                             dados.setPosDout(aux)
                             dados.setReg(contador)
@@ -176,31 +183,31 @@ def showData(linkedList,nivel):
         raise
 
 def menu():
-    print('(c) Carregar dados\n(e) Estatísticas da base de dados\n(i) Busca\n(s) Sobre\n(q) Sair')
+    print('(e) Estatísticas da base de dados\n(i) Busca\n(s) Sobre\n(q) Sair')
 
 def main():
     menu()
-    option = input('Escolha uma opção:', )
-    while True:
+    option = input('Escolha uma opção: ')
+    while option != 'q':
         if option == 'e':
             print('\n- A base de dados é formada por informações relativas a docentes,possuindo campos que informam o nível e a área de formação do mesmo;')
             print('- Os campos presentes na base são: Área de Graduação; Área de Especialização; Área de Mestrado; Área de Doutorado; Área de Pós-Doutorado;')
             print('- Nenhum dos registros possui Pós-Doutorado, porém a busca nessa área está disponível caso seja necessário em uso futuro;')
             print('- A base de dados possui um total de 1630 registros.\n')
-            main()
-            
+            break
+
         elif option == 'i':
             print()
             qualNv = ('GRADUAÇÃO','ESPECIALIZAÇÃO','MESTRADO','DOUTORADO','PÓS-DOUTORADO')
             qual = input('Qualificação(q p/ voltar):', ).upper()
             if qual == 'Q':
-                main()
-                
-            while qual not in qualNv:
+                break
+            
+            while qual not in qualNv and qual != 'Q':
                 print('\nQualificação Inválida\nDisponíveis: Graduação | Especialização | Mestrado | Doutorado | Pós-Doutorado')
-                qual = input('Nova Qualificação:', ).upper()
-                if qual == 'Q':
-                    main()
+                qual = input('Nova Qualificação(q p/ voltar):', ).upper()
+            if qual == 'Q':
+                break
             area = input('Área do Conhecimento:', )
             try:
                 damerau(qual,area)
@@ -215,12 +222,14 @@ def main():
             print('- A solução adotada foi a distância de edição normalizada Damerau-Levenshtein através da biblioteca textdistance;')
             print('- A área de formação fornecida pelo usuário é comparada com a(s) área de formação dos docentes presentes na base de dados;')
             print('- Os registros que atenderem um determinado requisito será visualizado pelo usuário com o nome do docente e a respectiva formação.\n')
-            main()
+            break
         
         elif option == 'q':
             break
 
         else:
-            pass    
+            print('\nOpção Inválida\n')  
+    if option != 'q':        
+        main()        
 
 
---
+++ readme.md
@@ -0,0 +1,5 @@
+- O programa faz uso de três bibliotecas: **csv**, **ast** e **textdistance**. As duas primeiras já vêm          previamente instaladas com o python3;
+
+- A biblioteca **textdistance** deve ser instalada com o comando 'pip install textdistance'. Mais informações
+  sobre: (https://pypi.org/project/textdistance/).
+
